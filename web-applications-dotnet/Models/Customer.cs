7338e697581df545df58fdd5b8fb3171e4f33be4
Erik Storås Sommer <erikssommer@gmail.com>
2021-09-08 14:36:37 +0200

    Attributes with capital

--- web-applications-dotnet/Models/Customer.cs
+++ web-applications-dotnet/Models/Customer.cs
@@ -2,8 +2,8 @@ namespace web_applications_dotnet.Models
 {
     public class Customer
     {
-        public int id { get; set; }
-        public string name { get; set; }
-        public string address { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
     }
 }
