7101b56173fde3bbd7a4befa71a3ed68a29df88b
Erik Storås Sommer <erikssommer@gmail.com>
2021-09-18 14:54:19 +0200

    Added login on client

---
+++ web-applications-dotnet/wwwroot/js/login.js
@@ -0,0 +1,19 @@
+function login() {
+
+    const brukernavnOK = validerBrukernavn($("#brukernavn").val());
+    const passordOK = validerPassord($("#passord").val());
+
+    if (brukernavnOK && passordOK) {
+        const customer = {
+            username: $("#brukernavn").val(),
+            password: $("#passord").val()
+        }
+        $.post("Customer/Login", customer, OK => {
+            if (OK) {
+                window.location.href = 'index.html';
+            } else {
+                $("#feil").html("Feil brukernavn eller passord");
+            }
+        }).fail(() => $("#feil").html("Feil på server - prøv igjen senere"));
+    }
+}
