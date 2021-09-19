4fafa8b4226962aeffcd0f3632b8966bb98e1bb9
Erik Storås Sommer <erikssommer@gmail.com>
2021-09-19 16:04:58 +0200

    Entity types can not be sealed

--- web-applications-dotnet/Models/PostOffices.cs
+++ web-applications-dotnet/Models/PostOffices.cs
@@ -4,13 +4,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace web_applications_dotnet.Models
 {
-    public sealed class PostOffices
+    public class PostOffices
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public string Postnr { get; set; }
         public string PostOffice { get; set; }
         
-        public List<Customers> Customers { get; set; }
+        public virtual List<Customers> Customers { get; set; }
     }
 }
